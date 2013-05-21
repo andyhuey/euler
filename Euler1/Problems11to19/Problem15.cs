251195ced6e3a5cc217cef3d60e4c35abc33218d
Andrew Huey <andyhuey@users.noreply.github.com>
2013-05-21 18:51:39 -0400

    problem 15 - simple solution

--- Euler1/Problems11to19/Problem15.cs
+++ Euler1/Problems11to19/Problem15.cs
@@ -7,25 +7,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Numerics;
 
 namespace Problems11to19
 {
 	public class Problem15
 	{
-		const int edge_length = 3;
+		const int edge_length = 20;
 
 		public long soln1()
 		{
 			// per http://www.robertdickau.com/lattices.html
-			long n = factorial(2 * edge_length);
-			long d = factorial(edge_length);
+            BigInteger n = factorial(2 * edge_length);
+            BigInteger d = factorial(edge_length);
 			d = d * d;
-			return n / d;
+			return (long)(n / d);
 
 		}
 
 		// quick & dirty
-		private long factorial(long n)
+        private BigInteger factorial(BigInteger n)
 		{
 			if (n <= 1)
 				return 1;
