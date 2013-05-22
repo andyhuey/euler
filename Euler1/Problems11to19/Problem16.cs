8dbc17a5b66726c878e1a29cbd98e8f53ff62977
Andrew Huey <andyhuey@users.noreply.github.com>
2013-05-22 19:52:23 -0400

    starting on problem 16

---
+++ Euler1/Problems11to19/Problem16.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * http://projecteuler.net/problem=16
+ * What is the sum of the digits of the number 2^1000?
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Problems11to19
+{
+    class Problem16
+    {
+        int max_power = 15;
+
+        public long soln1()
+        {
+            int pow;
+            int value = 2;
+
+            for (pow = 2; pow <= max_power; pow++)
+            {
+                value = value + value;
+            }
+
+            return value;
+        }
+    }
+}
