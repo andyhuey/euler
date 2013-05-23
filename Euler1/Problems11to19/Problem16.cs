a7153b5e99ff396bf5c9ea33b612e7501ecd6686
Andrew Huey <andyhuey@users.noreply.github.com>
2013-05-22 20:12:15 -0400

    problem 16

--- Euler1/Problems11to19/Problem16.cs
+++ Euler1/Problems11to19/Problem16.cs
@@ -13,19 +13,49 @@ namespace Problems11to19
 {
     class Problem16
     {
-        int max_power = 15;
+        int max_power = 1000;
 
         public long soln1()
         {
+            var sw = Stopwatch.StartNew();
             int pow;
-            int value = 2;
+            //int value = 2;
+            List<int> value = new List<int>() { 2 };
 
             for (pow = 2; pow <= max_power; pow++)
             {
-                value = value + value;
+                //value = value + value;
+                value = doubleIt(value);
             }
 
-            return value;
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
+
+            return value.Sum();
+        }
+
+        List<int> doubleIt(List<int> value)
+        {
+            List<int> newValue = new List<int>();
+            int sum;
+            int carry = 0;
+
+            for (int i = 0; i < value.Count; i++)
+            {
+                sum = value[i] + value[i] + carry;
+                carry = 0;
+                if (sum >= 10)
+                {
+                    carry = 1;
+                    sum -= 10;
+                }
+                newValue.Add(sum);
+            }
+
+            if (carry > 0)
+                newValue.Add(carry);
+
+            return newValue;
         }
     }
 }
