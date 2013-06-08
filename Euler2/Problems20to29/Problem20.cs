185019f2883d6103e9727682107a0d3865904c7b
Andrew Huey <andyhuey@users.noreply.github.com>
2013-06-08 17:21:16 -0400

    problem 20 - simple way

---
+++ Euler2/Problems20to29/Problem20.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * http://projecteuler.net/problem=20
+ * Find the sum of the digits in the number 100!
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Problems20to29
+{
+    class Problem20
+    {
+        public long soln1()
+        {
+            BigInteger biResult = 1;
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 100; i >= 1; i--)
+                biResult *= i;
+
+            string s = biResult.ToString();
+            Console.WriteLine(s);
+
+            long sumOfDigits = 0;
+            for (int i = 0; i < s.Length; i++)
+                sumOfDigits += Int32.Parse(s.Substring(i, 1));
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
+
+            return sumOfDigits;
+        }
+    }
+}
---
+++ Euler2/Problems20to29/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems20to29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProblem = new Problem20();
+            Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
+            //Console.WriteLine("the soln2() answer is {0:n0} or {0}", myProblem.soln2());
+
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+        }
+    }
+}
