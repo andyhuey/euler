2bc8343209e9b35ef27f1b10ce8b7efc587dd741
Andrew Huey <andyhuey@users.noreply.github.com>
2013-10-30 20:59:45 -0400

    problems 22 and 23

---
+++ Euler2/Problems20to29/Problem22.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * http://projecteuler.net/problem=22
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Problems20to29
+{
+    class Problem22
+    {
+        public long soln1()
+        {
+            string[] names_data = File.ReadAllLines(
+            @"C:\Users\andrew\Documents\Visual Studio 2012\Projects\Euler\Euler2\Problems20to29\names.txt");
+            List<string> names_list = new List<string>();
+            foreach (string line in names_data)
+            {
+                string[] names_line = line.Split(',');
+                //names_list.AddRange(names_line);
+                foreach (string name in names_line)
+                {
+                    string name1 = name.Trim().Trim('"');
+                    if (!string.IsNullOrEmpty(name1))
+                        names_list.Add(name1);
+                }
+            }
+
+            // sort the list
+            names_list.Sort();
+
+            // process the scores
+            long total_score = 0;
+            int pos = 0;
+
+            foreach (string name in names_list)
+            {
+                int ns = NameScore(name);
+                pos++;
+                total_score += (ns * pos);
+                //Console.WriteLine("{0}: {1}", name, ns);
+            }
+            return total_score;
+            //Console.WriteLine("The answer is {0}", total_score);
+            //Console.ReadLine();
+        } // end Main
+
+        private int NameScore(string name)
+        {
+            int score = 0;
+            foreach (char ch in name)
+            {
+                score += (int)ch - (int)'A' + 1;
+            }
+            return score;
+        }
+    }
+}
---
+++ Euler2/Problems20to29/Problem23.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * http://projecteuler.net/problem=23
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems20to29
+{
+    class Problem23
+    {
+        public long soln1()
+        {
+            // start by getting a list of 'abundant' numbers.
+            const int MAX_N = 28124;
+            string sdiv;
+            List<int> abundant_numbers = new List<int>();
+            for (int i = 1; i < MAX_N; i++)
+            {
+                if (is_abundant(i, sum_proper_divisors(i, out sdiv)))
+                {
+                    abundant_numbers.Add(i);
+                    //Console.Write("{0} ", i);
+                    //Console.WriteLine(sdiv);
+                }
+            }
+
+            // and now...
+            bool[] is_sum_of_abundant_nums = new bool[MAX_N];
+
+            for (int i = 0; i < MAX_N; i++)
+                is_sum_of_abundant_nums[i] = false;
+
+            for (int i = 0; i < abundant_numbers.Count - 1; i++)
+                for (int j = i; j < abundant_numbers.Count; j++)
+                {
+                    int x = abundant_numbers[i];
+                    int y = abundant_numbers[j];
+                    if (x + y < MAX_N)
+                        is_sum_of_abundant_nums[x + y] = true;
+                }
+
+            long final_sum = 0;
+            for (int i = 0; i < 28124; i++)
+                if (!is_sum_of_abundant_nums[i])
+                    final_sum += i;
+
+            return final_sum;
+            //Console.WriteLine("The answer is {0}", final_sum);
+            //Console.ReadLine();
+            // 4179871
+        }
+
+        static bool is_abundant(int n, int sum_div_n)
+        {
+            // "A number n is called deficient if the sum of its proper divisors is less than n 
+            //  and it is called abundant if this sum exceeds n."
+            return sum_div_n > n;
+        }
+
+        static int sum_proper_divisors(int n, out string div_list)
+        {
+            int i;
+            List<int> divisors = new List<int>() { 1 };
+            for (i = 2; i < Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    divisors.Add(i);
+                    divisors.Add(n / i);
+                }
+            }
+            // if n is a square...
+            if (i * i == n)
+                divisors.Add(i);
+            int sum_of_divisors = divisors.Sum();
+            div_list = string.Format("for n={0}: sum={1} / {2}", n, sum_of_divisors, string.Join(", ", divisors));
+            return sum_of_divisors;
+        }
+    }
+}
