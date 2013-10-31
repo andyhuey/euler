c3c3d2159c6ecf8f6f88387c2f84b5648934b465
Andrew Huey <andyhuey@users.noreply.github.com>
2013-10-31 16:55:59 -0400

    problem 25 - other solutions

--- Euler2/Problems20to29/Problem25.cs
+++ Euler2/Problems20to29/Problem25.cs
@@ -18,7 +18,6 @@ namespace Problems20to29
 {
     class Problem25
     {
-
         public long soln1()
         {
             // I wonder if brute force will work...
@@ -43,10 +42,48 @@ namespace Problems20to29
 
             Console.WriteLine("f({0}) = {1}", n, last_three_fib[2]);
             sw.Stop();
-            Console.WriteLine("elapsed: {0} sec", sw.Elapsed.Seconds);
-
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            // The answer is 4,782
             return n;
         }
 
+        public long soln2()
+        {
+            // from the thread - grimbal - works.
+            double n = Math.Ceiling((1000 - 1 + Math.Log10(Math.Sqrt(5))) / Math.Log10((1 + Math.Sqrt(5)) / 2));
+            return Convert.ToInt64(n);
+        }
+
+        public long soln3()
+        {
+            // from the thread - google this: (999+log(sqrt(5)))/log(phi) +1
+            double phi = (1 + Math.Sqrt(5)) / 2;
+            double n = (999 + Math.Log10(Math.Sqrt(5))) / Math.Log10(phi) + 1;
+            return Convert.ToInt64(Math.Floor(n));
+        }
+
+        public long soln4()
+        {
+            // from the thread - omgbentley - using LINQ - nifty, but slow.
+            var sw = Stopwatch.StartNew();
+            long ans = Fib().Select((x, i) => 
+                new { Term = i + 1, Value = x }).First(t => t.Value.ToString().Length == 1000).Term;
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            return ans;
+        }
+
+        IEnumerable<BigInteger> Fib()
+        {
+            BigInteger current = 0, next = 1, temp;
+
+            while (true)
+            {
+                yield return next;
+                temp = current + next;
+                current = next;
+                next = temp;
+            }
+        }
     }
 }
