f12be734161481205b8303a20797bc3fc5a95d82
Andrew Huey <andyhuey@users.noreply.github.com>
2014-04-07 15:49:43 -0400

    problem 28 complete (brute force)

--- Euler2/Problems20to29/Problem28.cs
+++ Euler2/Problems20to29/Problem28.cs
@@ -1,5 +1,7 @@
 ﻿/*
  * http://projecteuler.net/problem=28
+ * Number spiral diagonals
+ * The answer is: 669,171,001
  */
 using System;
 using System.Collections.Generic;
@@ -11,23 +13,68 @@ namespace Problems20to29
 {
     class Problem28
     {
-        const int GRID_SIZE = 5;
+        // grid size should be an odd number.
+        const int GRID_SIZE = 1001;
+        enum Direction { Right, Down, Left, Up };
 
         public long soln1()
         {
-            int[,] gridValues = new int[GRID_SIZE,GRID_SIZE];
+            // oops, this wouldn't actually work with negative indices, dummy:
+            //int[,] gridValues = new int[GRID_SIZE,GRID_SIZE];
             int x = 0, y = 0;
-            int direction = 1;
+            Direction dir = Direction.Right;
             int increment = 1;
+            int steps = 0;
             long sumOfDiags = 0;
             int counter = 1;
 
-            while (counter < GRID_SIZE * GRID_SIZE)
+            while (counter <= GRID_SIZE * GRID_SIZE)
             {
-                gridValues[x, y] = counter;
+                // set the cell value: (not really necessary)
+                //gridValues[x, y] = counter;
+                // is this cell on a diagonal?
                 if (Math.Abs(x) == Math.Abs(y))
                     sumOfDiags += counter;
-                // logic to move to next cell goes here...
+                // logic to move to next cell:
+                steps++;
+                switch (dir)
+                {
+                    case Direction.Right:
+                        x++;
+                        if (steps == increment)
+                        {
+                            steps = 0;
+                            dir = Direction.Down;
+                        }
+                        break;
+                    case Direction.Down:
+                        y--;
+                        if (steps == increment)
+                        {
+                            steps = 0;
+                            dir = Direction.Left;
+                            increment++;
+                        }
+                        break;
+                    case Direction.Left:
+                        x--;
+                        if (steps == increment)
+                        {
+                            steps = 0;
+                            dir = Direction.Up;
+                        }
+                        break;
+                    case Direction.Up:
+                        y++;
+                        if (steps == increment)
+                        {
+                            steps = 0;
+                            dir = Direction.Right;
+                            increment++;
+                        }
+                        break;
+
+                }
                 counter++;
             }
             return sumOfDiags;
