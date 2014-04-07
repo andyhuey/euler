3a6ba35e784479575e531f065ffc898a1c85d1e1
Andrew Huey <andyhuey@users.noreply.github.com>
2014-04-07 16:56:34 -0400

    problem 29 done

---
+++ Euler2/Problems20to29/Problem29.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * http://projecteuler.net/problem=29
+ * Distinct powers
+ * The answer is 9183.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems20to29
+{
+    class Problem29
+    {
+        public int soln1()
+        {
+            HashSet<double> powers = new HashSet<double>();
+            for (int a = 2; a <= 100; a++)
+                for (int b = 2; b <= 100; b++)
+                {
+                    powers.Add(Math.Pow(a, b));
+                }
+            return powers.Count();
+        }
+    }
+}
