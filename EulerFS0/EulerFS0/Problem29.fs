83c764e9c9ffe1ce3cc43c0a0abc959d4a4ff206
Andrew Huey <andyhuey@users.noreply.github.com>
2014-04-07 20:21:23 -0400

    problem 29 in F#.

---
+++ EulerFS0/EulerFS0/Problem29.fs
@@ -0,0 +1,8 @@
+﻿module Problem29
+// the answer is 9283
+let soln1 = 
+    //let nMax = 100
+    seq { for a in [2.0..100.0] do
+          for b in [2..100] -> pown a b }
+    |> Seq.distinct 
+    |> Seq.length
