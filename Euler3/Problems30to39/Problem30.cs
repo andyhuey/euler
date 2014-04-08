d77b7e09fcf5f17259eda9dc4901bc6eb3833200
Andrew Huey <andyhuey@users.noreply.github.com>
2014-04-07 20:21:47 -0400

    new project for problems 30-39

---
+++ Euler3/Problems30to39/Problem30.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problems30to39
+{
+    class Problem30
+    {
+        public int soln1()
+        {
+            return 0;
+        }
+    }
+}
