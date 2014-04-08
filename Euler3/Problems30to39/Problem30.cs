0cacbb18cf8c39bce2b1e2ba1e5f571bfd074215
Andrew Huey <andyhuey@users.noreply.github.com>
2014-04-07 21:28:09 -0400

    solved problem 30

--- Euler3/Problems30to39/Problem30.cs
+++ Euler3/Problems30to39/Problem30.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ * http://projecteuler.net/problem=30
+ * Digit fifth powers
+ * 
+ * Found: 4150
+ * Found: 4151
+ * Found: 54748
+ * Found: 92727
+ * Found: 93084
+ * Found: 194979
+ * The answer is 443,839
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +19,34 @@ namespace Problems30to39
 {
     class Problem30
     {
-        public int soln1()
+        public long soln1()
         {
-            return 0;
+            // my solution
+            long sumOfNumbers = 0;
+            for (long i = 10; i < Math.Pow(9, 5) * 6; i++)
+            {
+                string s = i.ToString();
+                long sumOfPowers = 0;
+                for (int ci = 0; ci < s.Length; ci++)
+                {
+                    sumOfPowers += (long)Math.Pow(Double.Parse(s.Substring(ci,1)), 5);
+                }
+                if (sumOfPowers == i)
+                {
+                    Console.WriteLine("Found: {0}", i);
+                    sumOfNumbers += sumOfPowers;
+                }
+            }
+            return sumOfNumbers;
+        }
+
+        public long soln2()
+        {
+            // from user adamsonic, 03 Apr 2014
+            return Enumerable.Range(2, 500000).Where(
+                t => t == t.ToString().ToArray().Select(
+                    digit => int.Parse(digit.ToString())).Aggregate(
+                    0, (acc, num) => acc + (int)Math.Pow(num, 5))).Sum();
         }
     }
 }
