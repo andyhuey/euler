fc90e9b98ca8ba6b343c4df23c23cdd71c3df63d
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-19 14:46:48 -0400

    problem 38 started

--- Euler3/Problems30to39/Program.cs
+++ Euler3/Problems30to39/Program.cs
@@ -10,7 +10,7 @@ namespace Problems30to39
     {
         static void Main(string[] args)
         {
-			var myProblem = new Problem37();
+			var myProblem = new Problem38();
             Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
 
             Console.WriteLine("Press enter...");
