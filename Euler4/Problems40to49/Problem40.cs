f0cd6f2b7a4087dacf066f440f0b8a508d154a3d
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-19 16:39:00 -0400

    set up project for problems 40-49

---
+++ Euler4/Problems40to49/Problem40.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * http://projecteuler.net/problem=40
+ * 
+ * Answer: 
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Problems40to49
+{
+    class Problem40
+    {
+        public long soln1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            return 0;
+
+        }
+    }
+}
---
+++ Euler4/Problems40to49/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems40to49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProblem = new Problem40();
+            Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
+
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+        }
+    }
+}
