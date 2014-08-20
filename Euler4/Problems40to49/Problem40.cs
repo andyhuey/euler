f41dcea9fdc691376eabf8d4eedf26fc5386b278
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-20 09:04:52 -0400

    problem 40 - thru to 6 digits

--- Euler4/Problems40to49/Problem40.cs
+++ Euler4/Problems40to49/Problem40.cs
@@ -19,7 +19,7 @@ namespace Problems40to49
 
             // let's start with just a string...
             StringBuilder sb = new StringBuilder();
-            for (int i = 1; i < 10000; i++)
+            for (int i = 1; i < 100000; i++)
             {
                 sb.Append(i);
             }
@@ -28,7 +28,7 @@ namespace Problems40to49
             //for (int i = 186; i <= 189; i++)
             //    Console.WriteLine(sb[i - 1]);
 
-            for (int i = 1; i < 2890; i++)
+            for (int i = 33000; i < 33100; i++)
             {
                 int sbi = sb[i - 1] - (int)'0';
                 if (sbi != getChampernowneDigit(i))
@@ -38,8 +38,8 @@ namespace Problems40to49
                 }
             }
 
-            //for (int i = 2880; i <= 2889; i++)
-            //    Console.WriteLine(getChampernowneDigit(i));
+            for (int i = 5888000; i < 5888890; i++)
+                Console.WriteLine(getChampernowneDigit(i));
 
             sw.Stop();
             Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
@@ -66,12 +66,26 @@ namespace Problems40to49
                 dr = (dn - 190) % 3;
                 return d.ToString()[dr] - (int)'0';
             }
+            if (dn < 38890)
+            {
+                d = 1000 + (dn - 2890) / 4;
+                dr = (dn - 2890) % 4;
+                return d.ToString()[dr] - (int)'0';
+            }
+            if (dn < 488890)
+            {
+                d = 10000 + (dn - 38890) / 5;
+                dr = (dn - 38890) % 5;
+                return d.ToString()[dr] - (int)'0';
+            }
+            if (dn < 5888890)
+            {
+                d = 100000 + (dn - 488890) / 6;
+                dr = (dn - 488890) % 6;
+                return d.ToString()[dr] - (int)'0';
+            }
             throw new ArgumentOutOfRangeException("dn", "dn is too large.");
         }
 
-        private bool isEven(int i)
-        {
-            return i % 2 == 0;
-        }
     }
 }
