fdd7e8e46fcee164cafd4f7aaf82aeb901b8f2e6
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-21 15:31:21 -0400

    problem 43 solved (brute force)

--- Euler4/Problems40to49/Problem43.cs
+++ Euler4/Problems40to49/Problem43.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * http://projecteuler.net/problem=43
  * Sub-string divisibility
- * Answer: 
+ * The answer is 16,695,334,890 or 16695334890
  */
 using System;
 using System.Collections.Generic;
@@ -14,51 +14,72 @@ namespace Problems40to49
 {
     class Problem43
     {
-        IEnumerable<string> allPandig;
+        List<string> allPandig;
 
         public long soln1()
         {
-            int ans = 0;
+            long ans = 0;
             var sw = Stopwatch.StartNew();
 
-            allPandig = genPandig("").ToList();
+            genPandig("");
 
-            //Console.WriteLine("generated {0} numbers.", allPandig.Count);
+            // generated 3265920 numbers.
+            Console.WriteLine("generated {0} numbers.", allPandig.Count);
+
             foreach (string s in allPandig)
-                Console.WriteLine(s);
+            {
+                if (meetsCriteria(s))
+                {
+                    ans += Convert.ToInt64(s);
+                    Console.WriteLine(s);
+                }
+            }
 
             sw.Stop();
             Console.WriteLine("elapsed: {0} sec", sw.Elapsed.Seconds);
             return ans;
         }
 
-        private IEnumerable<string> genPandig(string s)
+        private void genPandig(string s)
         {
             //Console.WriteLine("Called with {0}.", s);
             // kick it off with the digits from 1-9
             if (s == "")
             {
-                //allPandig = new List<string>();
+                allPandig = new List<string>();
                 for (char ch = '1'; ch <= '9'; ch++)
                 {
-                    foreach (var x in genPandig(ch.ToString())) yield return x;
+                    genPandig(ch.ToString());
                 }
-                //return;
+                return;
             }
             // if it's 10 digits, we're done.
             if (s.Length == 10)
             {
-                yield return s;
-                //allPandig.Add(s);
-                //return;
+                allPandig.Add(s);
+                return;
             }
             // otherwise, recurse.
             for (char ch = '0'; ch <= '9'; ch++)
             {
                 if (s.IndexOf(ch) >= 0)     // already used.
                     continue;
-                foreach (var x in genPandig(s + ch)) yield return x;
+                genPandig(s + ch);
             }
+        }
+
+        private bool meetsCriteria(string s)
+        {
+            // these are strange criteria, but ok...
+            int substr;
+            int[] divis_by = new int[7] { 2, 3, 5, 7, 11, 13, 17 };
+            for (int i = 0; i < 7; i++)
+            {
+                substr = Int32.Parse(s.Substring(i + 1, 3));
+                if (substr % divis_by[i] != 0)
+                    return false;
+            }
+            return true;
         }
     }
 }
