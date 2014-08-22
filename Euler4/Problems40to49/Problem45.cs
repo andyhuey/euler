ebca326b8a598963fed06132350931c317d5fbbb
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-22 15:41:05 -0400

    problem 45 solved

--- Euler4/Problems40to49/Problem45.cs
+++ Euler4/Problems40to49/Problem45.cs
@@ -1,7 +1,9 @@
 ﻿/*
  * http://projecteuler.net/problem=45
  * Triangular, pentagonal, and hexagonal
- * (got up to 200,000,000 without hitting it.)
+ * Find the next triangle number that is also pentagonal and hexagonal.
+ * T(27693)=1533776805 is T, P, and H.
+ * The answer is 1,533,776,805 or 1533776805
  */
 using System;
 using System.Collections.Generic;
@@ -22,23 +24,26 @@ namespace Problems40to49
 
             int i = startAt;
             bool found = false;
-            long t;
+            long h;
             do
-            {
-                t = triangleNum(i);
-                if (isPentagonNum(t) && isHexagonNum(t))
+            {   // Note: all hexagonal numbers are triangle numbers.
+                // See http://www.mathblog.dk/project-euler-45-next-triangle-pentagonal-hexagonal-number/
+                //t = triangleNum(i);
+                //if (isPentagonNum(t) && isHexagonNum(t))
+                h = hexagonNum(i);
+                if (isPentagonNum(h))
                 {
-                    Console.WriteLine("T{0}={1} is T, P, and H.", i, t);
+                    Console.WriteLine("T({0})={1} is T, P, and H.", i, h);
                     found = true;
                 }
-                if (i % 10000 == 0)
+                if (i % 1000000 == 0)
                     Console.WriteLine("i={0:n0}", i);
                 i++;
             } while (!found);
 
             sw.Stop();
             Console.WriteLine("elapsed: {0} sec", sw.Elapsed.Seconds);
-            return t;
+            return h;
         }
         private long triangleNum(int n)
         {
