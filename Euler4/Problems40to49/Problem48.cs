a96af04cd7c19262d69bfad331fb54b5a778e6b6
Andrew Huey <andyhuey@users.noreply.github.com>
2014-08-29 15:47:02 -0400

    problem 48 - brute force solution

---
+++ Euler4/Problems40to49/Problem48.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * http://projecteuler.net/problem=48
+ * Self Powers
+ * 1..10: 10,405,071,317
+ * The answer is 9110846700.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Numerics;
+
+namespace Problems40to49
+{
+    class Problem48
+    {
+        public long soln1()
+        {
+            var sw = Stopwatch.StartNew();
+            long n = 0;
+            BigInteger bigSum = 0;
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                bigSum += BigInteger.Pow(i, i);
+            }
+            //n = (long)bigSum;
+            n = (long)(bigSum % BigInteger.Pow(10, 10));
+
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            return n;
+        }
+    }
+}
--- Euler4/Problems40to49/Program.cs
+++ Euler4/Problems40to49/Program.cs
@@ -10,7 +10,7 @@ namespace Problems40to49
     {
         static void Main(string[] args)
         {
-            var myProblem = new Problem47();
+            var myProblem = new Problem48();
             Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
 
             Console.WriteLine("Press enter...");
