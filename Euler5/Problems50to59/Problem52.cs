ed53c52a2bf7c0490b5ff415340cab80978770ea
Andrew Huey <andyhuey@users.noreply.github.com>
2014-09-24 14:00:46 -0400

    problem 52 solved

---
+++ Euler5/Problems50to59/Problem52.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * https://projecteuler.net/problem=52
+ * Permuted multiples
+ * The answer is 142,857
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems50to59
+{
+    class Problem52
+    {
+        const int MAX_M = 6;
+        const long MAX_N = long.MaxValue / MAX_M;
+
+        public long soln1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            long n = 0;
+            while (++n < MAX_N)
+            {
+                int m;
+                for (m = 2; m <= MAX_M; m++)
+                {
+                    if (!isPermutation(n, n * m))
+                        break;
+                    Console.WriteLine("{0} * {1} = {2}", n, m, n * m);
+                }
+                if (m > MAX_M)
+                    return n;
+            }
+
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            return 0;
+        }
+
+        private bool isPermutation(long n1, long n2)
+        {
+            // this isn't a terribly efficient way of doing this.
+            // some interesting solutions: http://stackoverflow.com/q/3669970/301677 & http://stackoverflow.com/q/50098/301677
+            List<char> ca1 = new List<char>(n1.ToString());
+            List<char> ca2 = new List<char>(n2.ToString());
+            if (ca1.Count != ca2.Count)
+                return false;
+            ca1.Sort();
+            ca2.Sort();
+            return ca1.SequenceEqual(ca2);
+        }
+    }
+}
