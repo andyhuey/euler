fedc31272a88fe861dad1ec647b1513329ab6202
Andrew Huey <andyhuey@users.noreply.github.com>
2014-09-25 11:56:33 -0400

    messing with problem 53.

--- Euler5/Problems50to59/Problem53.cs
+++ Euler5/Problems50to59/Problem53.cs
@@ -1,7 +1,10 @@
 ﻿/*
  * https://projecteuler.net/problem=53
  * Combinatoric selections
+ * see http://en.wikipedia.org/wiki/Binomial_coefficient
  * The answer is 4,075
+ * 1 sec for the BigInteger solution w/factorials.
+ * 17 sec for the recursive solution.
  */
 using System;
 using System.Collections.Generic;
@@ -16,6 +19,7 @@ namespace Problems50to59
     class Problem53
     {
         const int C_MAX = 1000000;
+        Dictionary<Tuple<int, int>, bool> isComboGT1M = new Dictionary<Tuple<int, int>, bool>();
 
         public long soln1()
         {
@@ -28,22 +32,36 @@ namespace Problems50to59
             {
                 for (int r = 1; r <= n; r++)
                 {
-                    if (combinations(n, r) > C_MAX)
+                    BigInteger c = combinations(n, r);
+                    if (c > C_MAX)
                     {
-                        Console.WriteLine("n={0}, r={1}", n, r);
+                        //isComboGT1M.Add(Tuple.Create(n, r), true);  // remember that this is > 1M
+                        Console.WriteLine("n={0}, r={1} -> {2}", n, r, c);
                         count++;
                     }
                 }
             }
 
             sw.Stop();
-            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.Milliseconds);
+            Console.WriteLine("elapsed: {0} sec", sw.Elapsed.Seconds);
             return count;
         }
 
+        //private long combinations(int n, int k)
+        //{
+        //    // n choose k, recursive, but short-circuit when > 1,000,000
+        //    if (k == 0 || n == k)
+        //        return 1;
+        //    // just return 1,000,001 if we know c(n-1,k-1) is > 1M.
+        //    var key = Tuple.Create(n-1, k-1);
+        //    if (isComboGT1M.ContainsKey(key) && isComboGT1M[key])
+        //        return C_MAX + 1;
+        //    return combinations(n - 1, k - 1) + combinations(n - 1, k);
+        //}
+
         private BigInteger combinations(int n, int r)
         {
-            // choose r from n
+            // brute force: choose r from n
             BigInteger c = factorial(n) / (factorial(r) * factorial(n - r));
             return c;
         }
