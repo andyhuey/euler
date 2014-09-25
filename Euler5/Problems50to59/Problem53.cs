fad2e969b165eccd1fe456d890fec22d80924fb3
Andrew Huey <andyhuey@users.noreply.github.com>
2014-09-25 12:02:57 -0400

    messing with problem 53.

--- Euler5/Problems50to59/Problem53.cs
+++ Euler5/Problems50to59/Problem53.cs
@@ -3,8 +3,9 @@
  * Combinatoric selections
  * see http://en.wikipedia.org/wiki/Binomial_coefficient
  * The answer is 4,075
- * 1 sec for the BigInteger solution w/factorials.
- * 17 sec for the recursive solution.
+ * < 1 sec for the BigInteger solution w/factorials.
+ * 17 sec for the original recursive solution.
+ * < 1 sec for the revised recursive solution.
  */
 using System;
 using System.Collections.Generic;
@@ -19,7 +20,7 @@ namespace Problems50to59
     class Problem53
     {
         const int C_MAX = 1000000;
-        Dictionary<Tuple<int, int>, bool> isComboGT1M = new Dictionary<Tuple<int, int>, bool>();
+        Dictionary<Tuple<int, int>, long> comboVal = new Dictionary<Tuple<int, int>, long>();
 
         public long soln1()
         {
@@ -35,7 +36,6 @@ namespace Problems50to59
                     BigInteger c = combinations(n, r);
                     if (c > C_MAX)
                     {
-                        //isComboGT1M.Add(Tuple.Create(n, r), true);  // remember that this is > 1M
                         Console.WriteLine("n={0}, r={1} -> {2}", n, r, c);
                         count++;
                     }
@@ -47,24 +47,34 @@ namespace Problems50to59
             return count;
         }
 
-        //private long combinations(int n, int k)
-        //{
-        //    // n choose k, recursive, but short-circuit when > 1,000,000
-        //    if (k == 0 || n == k)
-        //        return 1;
-        //    // just return 1,000,001 if we know c(n-1,k-1) is > 1M.
-        //    var key = Tuple.Create(n-1, k-1);
-        //    if (isComboGT1M.ContainsKey(key) && isComboGT1M[key])
-        //        return C_MAX + 1;
-        //    return combinations(n - 1, k - 1) + combinations(n - 1, k);
-        //}
-
-        private BigInteger combinations(int n, int r)
+        private long combinations(int n, int k)
         {
-            // brute force: choose r from n
-            BigInteger c = factorial(n) / (factorial(r) * factorial(n - r));
-            return c;
+            // n choose k, recursive, but short-circuit when > 1,000,000
+            var key = Tuple.Create(n, k);
+            if (comboVal.ContainsKey(key))
+                return comboVal[key];
+            if (k == 0 || n == k)
+            {
+                comboVal[key] = 1;
+                return 1;
+            }
+            long left = combinations(n - 1, k - 1);
+            long right = combinations(n - 1, k);
+            if (left > C_MAX || right > C_MAX)
+            {
+                comboVal[key] = C_MAX + 1;
+                return C_MAX + 1;
+            }
+            comboVal[key] = left + right;
+            return left + right;
         }
+
+        //private BigInteger combinations(int n, int r)
+        //{
+        //    // brute force: choose r from n
+        //    BigInteger c = factorial(n) / (factorial(r) * factorial(n - r));
+        //    return c;
+        //}
 
         private BigInteger factorial(BigInteger n)
         {
