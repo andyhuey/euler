7b8fb94af2ed98dafd5ac8464fc6486d2cfdab97
Andrew Huey <andyhuey@users.noreply.github.com>
2014-09-25 12:57:31 -0400

    messing with problem 53; adding overview pdf.

--- Euler5/Problems50to59/Problem53.cs
+++ Euler5/Problems50to59/Problem53.cs
@@ -3,9 +3,11 @@
  * Combinatoric selections
  * see http://en.wikipedia.org/wiki/Binomial_coefficient
  * The answer is 4,075
+ * 
  * < 1 sec for the BigInteger solution w/factorials.
  * 17 sec for the original recursive solution.
- * < 1 sec for the revised recursive solution.
+ * 1624 ms for the revised recursive solution.
+ * 1012 ms with array instead of dictionary.
  */
 using System;
 using System.Collections.Generic;
@@ -20,7 +22,8 @@ namespace Problems50to59
     class Problem53
     {
         const int C_MAX = 1000000;
-        Dictionary<Tuple<int, int>, long> comboVal = new Dictionary<Tuple<int, int>, long>();
+        //Dictionary<Tuple<int, int>, long> comboVal = new Dictionary<Tuple<int, int>, long>();
+        long[,] comboVal = new long[101, 101];
 
         public long soln1()
         {
@@ -33,7 +36,7 @@ namespace Problems50to59
             {
                 for (int r = 1; r <= n; r++)
                 {
-                    BigInteger c = combinations(n, r);
+                    long c = combinations(n, r);
                     if (c > C_MAX)
                     {
                         Console.WriteLine("n={0}, r={1} -> {2}", n, r, c);
@@ -43,29 +46,30 @@ namespace Problems50to59
             }
 
             sw.Stop();
-            Console.WriteLine("elapsed: {0} sec", sw.Elapsed.Seconds);
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
             return count;
         }
 
         private long combinations(int n, int k)
         {
             // n choose k, recursive, but short-circuit when > 1,000,000
-            var key = Tuple.Create(n, k);
-            if (comboVal.ContainsKey(key))
-                return comboVal[key];
+            if (comboVal[n, k] > 0)
+            {
+                return comboVal[n, k];
+            }
             if (k == 0 || n == k)
             {
-                comboVal[key] = 1;
+                comboVal[n, k] = 1;
                 return 1;
             }
             long left = combinations(n - 1, k - 1);
             long right = combinations(n - 1, k);
             if (left > C_MAX || right > C_MAX)
             {
-                comboVal[key] = C_MAX + 1;
+                comboVal[n, k] = C_MAX + 1;
                 return C_MAX + 1;
             }
-            comboVal[key] = left + right;
+            comboVal[n, k] = left + right;
             return left + right;
         }
 
