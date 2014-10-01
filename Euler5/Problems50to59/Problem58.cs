3a8f11c82232d278e2408e95402b05445053a0c1
Andrew Huey <andyhuey@users.noreply.github.com>
2014-10-01 08:37:09 -0400

    tried another user's solution to problem 58

--- Euler5/Problems50to59/Problem58.cs
+++ Euler5/Problems50to59/Problem58.cs
@@ -5,6 +5,7 @@
  * The answer is 26,241
  */
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -16,12 +17,6 @@ namespace Problems50to59
     class Problem58
     {
         const int MAX_GRID = 30000;
-        Random r;
-
-        public Problem58()
-        {
-            r = new Random();
-        }
 
         public long soln1()
         {
@@ -63,7 +58,7 @@ namespace Problems50to59
                         nPrimes++;
                 }
                 double ratio = (double)nPrimes / nDiagCells;
-                Console.WriteLine("For grid size {0}, result is {1}/{2} or {3:0}%", n, nPrimes, nDiagCells, ratio * 100);
+                //Console.WriteLine("For grid size {0}, result is {1}/{2} or {3:0}%", n, nPrimes, nDiagCells, ratio * 100);
                 //Console.ReadLine();
                 if (ratio < 0.10)
                     return n;
@@ -85,5 +80,43 @@ namespace Problems50to59
             return true;
         }
 
+        // solution below is from user skyivair, 15 May 2014.
+        // (using a sieve actually takes longer than just checking one at a time.)
+        const int N = 30001;
+        static BitArray primes;
+
+        public long soln2()
+        {
+            var sw = Stopwatch.StartNew();
+
+            PrimeSieve(MAX_GRID * MAX_GRID);
+            int gridSize = SpiralCalc();
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
+            return gridSize;
+        }
+
+        private void PrimeSieve(int n)
+        {
+            primes = new BitArray(n + 1, true);
+            primes[0] = primes[1] = false;
+            int m = (int)Math.Sqrt(n);
+            for (int i = 2; i <= m; i++)
+                if (primes[i])
+                    for (int k = i << 1; k <= n; k += i)
+                        primes[k] = false;
+        }
+
+      private int SpiralCalc()
+      {
+          for (int n = 0, i = 1, k = 2; ; i += k, k += 2)
+          {
+              if (primes[i += k]) n++;
+              if (primes[i += k]) n++;
+              if (primes[i += k]) n++;
+              if (n * 5 <= k) return k + 1;
+          }
+      }
     }
 }
