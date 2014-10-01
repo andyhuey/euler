400bbbf1d1f3f29500bd71092f140191dcec47ab
Andrew Huey <andyhuey@users.noreply.github.com>
2014-10-01 15:18:53 -0400

    start on problem 60

---
+++ Euler6/Problems60to69/Problem60.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * https://projecteuler.net/problem=60
+ * Prime Pair Sets
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems60to69
+{
+    class Problem60
+    {
+        bool[] primes;
+        const int nPrimeMax = 1000000;
+
+        public long soln1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            getPrimes();
+
+            Console.WriteLine(isPrimePairSet(new int[] { 3, 7, 109, 673 }));
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
+            return 0;
+        }
+
+        private bool isPrimePairSet(int[] myset)
+        {
+            for (int i = 0; i < myset.Length; i++)
+            {
+                if (!isPrime(myset[i]))
+                    return false;
+                for (int j = i + 1; j < myset.Length; j++)
+                {
+                    string si = myset[i].ToString();
+                    string sj = myset[j].ToString();
+                    int ij = Int32.Parse(si + sj);
+                    int ji = Int32.Parse(sj + si);
+                    if (!isPrime(ij) || !isPrime(ji))
+                        return false;
+                    Console.WriteLine("{0}, {1}", ij, ji);
+                }
+            }
+            return true;
+        }
+
+        private bool isPrime(int n)
+        {
+            return primes[n];
+        }
+
+        private void getPrimes()
+        {
+            // get primes
+            primes = new bool[nPrimeMax];
+            int p = 2;
+            int sqrt_max = (int)Math.Floor(Math.Sqrt(nPrimeMax));
+
+            // initialize all to true
+            for (int i = 2; i < nPrimeMax; i++)
+                primes[i] = true;
+
+            while (p <= sqrt_max)
+            {
+                // cross out all the multiple of p.
+                for (int i = p * p; i < nPrimeMax; i += p)
+                {
+                    primes[i] = false;
+                }
+
+                // get the next p.
+                do
+                {
+                    p++;
+                } while (!primes[p]);
+            }
+        }
+
+    }
+}
---
+++ Euler6/Problems60to69/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems60to69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProblem = new Problem60();
+            Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
+
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+        }
+    }
+}
