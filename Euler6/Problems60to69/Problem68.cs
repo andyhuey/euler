536c5979390c5fa650bb841577e3c5607a5a7d88
Andrew Huey <andyhuey@users.noreply.github.com>
2015-04-02 11:03:45 -0400

    problem 68 solved.

--- Euler6/Problems60to69/Problem68.cs
+++ Euler6/Problems60to69/Problem68.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * http://projecteuler.net/problem=68
  * Magic 5-gon ring
- * (starting with 3-gon ring)
+ * The max digit string is 6531031914842725.
  */
 using System;
 using System.Collections.Generic;
@@ -12,12 +12,13 @@ using System.Threading.Tasks;
 
 namespace Problems60to69
 {
-    class ThreeGonRing
+    class FiveGonRing
     {
         int[] node;
-        public ThreeGonRing (int[] node)
+        public FiveGonRing (int[] node)
 	    {
-            Debug.Assert(node.Length == 6);
+            // we need an array of ten numbers to start.
+            Debug.Assert(node.Length == 10);
             this.node = node;
 	    }
 
@@ -30,16 +31,23 @@ namespace Problems60to69
                 case 1:
                     return new int[3] { node[3], node[2], node[4] };
                 case 2:
-                    return new int[3] { node[5], node[4], node[1] };
+                    return new int[3] { node[5], node[4], node[6] };
+                case 3:
+                    return new int[3] { node[7], node[6], node[8] };
+                case 4:
+                    return new int[3] { node[9], node[8], node[1] };
             }
-            throw new ArgumentOutOfRangeException("n must be in 0-2.");
+            throw new ArgumentOutOfRangeException("n must be in 0-4.");
         }
 
         public int getMagicRingSum()
         {
-            // sum of each tuple must be the same. return -1 if not magic
+            // sum of each tuple must be the same. return -1 if not magic.
             int s1 = getTuple(0).Sum();
-            if (s1 == getTuple(1).Sum() && s1 == getTuple(2).Sum())
+            if (s1 == getTuple(1).Sum()
+                && s1 == getTuple(2).Sum()
+                && s1 == getTuple(3).Sum()
+                && s1 == getTuple(4).Sum())
                 return s1;
             else
                 return -1;
@@ -47,8 +55,8 @@ namespace Problems60to69
 
         public bool isValidRing()
         {
-            // must be one each of 1..6.
-            for (int i = 1; i <= 6; i++)
+            // must be one each of 1..10.
+            for (int i = 1; i <= 10; i++)
             {
                 if (node.Where(n => n == i).Count() != 1)
                     return false;
@@ -56,17 +64,30 @@ namespace Problems60to69
             return true;
         }
 
-        public bool isRotationOf(ThreeGonRing ring)
+        private bool checkSeqEq(int[] t1, int[] t2, FiveGonRing ring)
         {
-            // 0,1,2 -> 1,2,0
-            if (this.getTuple(0).SequenceEqual(ring.getTuple(1))
-                && this.getTuple(1).SequenceEqual(ring.getTuple(2))
-                && this.getTuple(2).SequenceEqual(ring.getTuple(0)))
+            Debug.Assert(t1.Length == t2.Length);
+            for (int i = 0; i < t1.Length; i++)
+                if (!this.getTuple(t1[i]).SequenceEqual(ring.getTuple(t2[i])))
+                    return false;
+            return true;
+        }
+
+        public bool isRotationOf(FiveGonRing ring)
+        {
+            // I'm sure there's a much better way of doing this...
+            int[] t1 = new int[] { 0, 1, 2, 3, 4 };
+            // 0,1,2,3,4 -> 1,2,3,4,0
+            if (this.checkSeqEq(t1, new int[] { 1, 2, 3, 4, 0 }, ring))
                 return true;
-            // 0,1,2 -> 2,0,1
-            if (this.getTuple(0).SequenceEqual(ring.getTuple(2))
-                && this.getTuple(1).SequenceEqual(ring.getTuple(0))
-                && this.getTuple(2).SequenceEqual(ring.getTuple(1)))
+            // 0,1,2,3,4 -> 2,3,4,0,1
+            if (this.checkSeqEq(t1, new int[] { 2, 3, 4, 0, 1 }, ring))
+                return true;
+            // 0,1,2,3,4 -> 3,4,0,1,2
+            if (this.checkSeqEq(t1, new int[] { 3, 4, 0, 1, 2 }, ring))
+                return true;
+            // 0,1,2,3,4 -> 4,0,1,2,3
+            if (this.checkSeqEq(t1, new int[] { 4, 0, 1, 2, 3 }, ring))
                 return true;
             // otherwise:
             return false;
@@ -74,17 +95,22 @@ namespace Problems60to69
 
         public string getDigitString()
         {
-            return string.Join<int>("", this.getTuple(0)) + 
-                string.Join<int>("", this.getTuple(1)) + 
-                string.Join<int>("", this.getTuple(2));
+            // get a string with all the digits, in order.
+            return string.Join<int>("", this.getTuple(0)) +
+                string.Join<int>("", this.getTuple(1)) +
+                string.Join<int>("", this.getTuple(2)) +
+                string.Join<int>("", this.getTuple(3)) +
+                string.Join<int>("", this.getTuple(4));
         }
 
         public override string ToString()
         {
-            //return base.ToString();
+            // a nicely-formatted version of the data.
             return string.Join<int>(", ", this.getTuple(0)) + "; " +
                 string.Join<int>(", ", this.getTuple(1)) + "; " +
-                string.Join<int>(", ", this.getTuple(2));
+                string.Join<int>(", ", this.getTuple(2)) + "; " +
+                string.Join<int>(", ", this.getTuple(3)) + "; " +
+                string.Join<int>(", ", this.getTuple(4));
             
         }
     }
@@ -111,12 +137,14 @@ namespace Problems60to69
             List<int> p1 = new List<int>();
             int nPerms = 0;
             int nMagic = 0;
-            List<ThreeGonRing> magicRings = new List<ThreeGonRing>();
+            List<FiveGonRing> magicRings = new List<FiveGonRing>();
 
+            //Console.WriteLine("There are {0:n0} permutations to consider.", getAllPerms(p1).Count());
+            
             foreach (var p in getAllPerms(p1))
             {
                 //Console.WriteLine(string.Join<int>(", ", p));
-                var ring = new ThreeGonRing(p.ToArray());
+                var ring = new FiveGonRing(p.ToArray());
                 if (!ring.isValidRing())
                 {
                     throw new Exception(string.Format("{0} is not a valid ring.", ring.ToString()));
@@ -133,6 +161,9 @@ namespace Problems60to69
                 }
                 nPerms++;
             }
+
+            // get rid of any rings where the length of the digit string isn't 16.
+            magicRings.RemoveAll(x => x.getDigitString().Length != 16);
 
             foreach(var ring in magicRings.OrderBy(x => x.getMagicRingSum()))
             {
@@ -152,9 +183,9 @@ namespace Problems60to69
 
         private IEnumerable<List<int>> getAllPerms(List<int> perm)
         {
-            if (perm.Count < 6)
+            if (perm.Count < 10)
             {
-                for (int i = 1; i <= 6; i++)
+                for (int i = 1; i <= 10; i++)
                 {
                     if (!perm.Contains(i))
                     {
