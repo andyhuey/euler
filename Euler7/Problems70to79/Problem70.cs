4fffb440c21362888877fbd4f237bd1ea9303689
Andrew Huey <andyhuey@users.noreply.github.com>
2015-04-03 17:00:45 -0400

    solved problem 70

---
+++ Euler7/Problems70to79/Problem70.cs
@@ -0,0 +1,110 @@
+﻿/*
+ * http://projecteuler.net/problem=70
+ * Totient permutation
+ * (copied from problem 69.)
+ * The answer is 8,319,823.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems70to79
+{
+    class Problem70
+    {
+        const int nPrimeMax = 10000000;
+        const int MAX_N = 10000000;
+        bool[] primes;
+
+        public long soln1()
+        {
+            long n_for_min_n_over_phi_n = 0;
+            float min_n_over_phi_n = MAX_N;
+
+            List<int>[] primeFactors = new List<int>[MAX_N + 1];
+
+            getPrimes();
+            IEnumerable<int> lstPrimes = Enumerable.Range(2, nPrimeMax - 2).Where(x => primes[x]);
+            Console.WriteLine("Got {0} primes.", lstPrimes.Count());
+
+            foreach (int n in lstPrimes)
+            {
+                long n2 = n;
+                int i = 1;
+                while (n2 <= MAX_N)
+                {
+                    if (primeFactors[n2] == null)
+                        primeFactors[n2] = new List<int>();
+                    primeFactors[n2].Add(n);
+                    i++;
+                    n2 = n * i;
+                }
+            }
+            Console.WriteLine("Done filling in prime factor array.");
+
+            for (int n = 2; n <= MAX_N; n++)
+            {
+                //float denom = 1;
+                float phi_n = n;
+                foreach (var p in primeFactors[n])
+                    phi_n *= (1 - (float)1 / p);
+                
+                // only want numbers where phi(n) is a permutation of n.
+                if (!isPermutation(n, (long)phi_n))
+                    continue;
+                
+                float n_over_phi_n = (float)n / phi_n;
+                if (n_over_phi_n < min_n_over_phi_n)
+                {
+                    min_n_over_phi_n = n_over_phi_n;
+                    n_for_min_n_over_phi_n = n;
+                    Console.WriteLine("For n={0}, phi(n)={1} n/phi(n)={2:n6}", n, phi_n, n_over_phi_n);
+                }
+            }
+
+            return n_for_min_n_over_phi_n;
+        }
+
+        // prime method copied from problem 60.
+        private void getPrimes()
+        {
+            // get primes
+            primes = new bool[nPrimeMax];
+            int p = 2;
+            int sqrt_max = (int)Math.Floor(Math.Sqrt(nPrimeMax));
+
+            // initialize all to true
+            for (int i = 2; i < nPrimeMax; i++)
+                primes[i] = true;
+
+            while (p <= sqrt_max)
+            {
+                // cross out all the multiple of p.
+                for (int i = p * p; i < nPrimeMax; i += p)
+                {
+                    primes[i] = false;
+                }
+
+                // get the next p.
+                do
+                {
+                    p++;
+                } while (!primes[p]);
+            }
+        }
+
+        private bool isPermutation(long n1, long n2)
+        {
+            // from problem 52
+            List<char> ca1 = new List<char>(n1.ToString());
+            List<char> ca2 = new List<char>(n2.ToString());
+            if (ca1.Count != ca2.Count)
+                return false;
+            ca1.Sort();
+            ca2.Sort();
+            return ca1.SequenceEqual(ca2);
+        }
+    }
+}
---
+++ Euler7/Problems70to79/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems70to79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+            var myProblem = new Problem70();
+            Console.WriteLine("The answer is {0:n0} or {0}", myProblem.soln1());
+
+            sw.Stop();
+            Console.WriteLine("elapsed: {0} ms", sw.Elapsed.TotalMilliseconds);
+            Console.WriteLine("Press enter...");
+            Console.ReadLine();
+        }
+    }
+}
