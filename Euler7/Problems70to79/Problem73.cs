c745672ef325a0af48550b8d9f81a1e4d453f22e
Andrew Huey <andyhuey@users.noreply.github.com>
2023-10-28 16:17:40 -0400

    more on problem 73

--- Euler7/Problems70to79/Problem73.cs
+++ Euler7/Problems70to79/Problem73.cs
@@ -50,5 +50,32 @@ namespace Problems70to79
             }
             return nCount;
         }
+
+        public long soln2()
+        {
+            // from the PDF...
+            // "Farey Sequences"
+            // I don't understand this...
+            int limit = 10000;
+            int a = 1;
+            int b = 3;
+            int c = 3333;
+            int d = 9998;
+            int count = 0;
+
+            while (!(c == 1 && d == 2))
+            {
+                count++;
+                int k = (limit + b) / d;
+                int e = k * c - a;
+                int f = k * d - b;
+                a = c;
+                b = d;
+                c = e;
+                d = f;
+            }
+            return count;
+
+        }
     }
 }
