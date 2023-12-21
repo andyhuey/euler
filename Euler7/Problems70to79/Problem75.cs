b4ec6c5d7f147980e6b844a29e054457fa246ff4
Andrew Huey <andyhuey@users.noreply.github.com>
2023-12-21 16:43:03 -0500

    problem 75 - working on soln2

--- Euler7/Problems70to79/Problem75.cs
+++ Euler7/Problems70to79/Problem75.cs
@@ -5,6 +5,7 @@
  */
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,10 @@ namespace Problems70to79
         public static void Run()
         {
             var myProblem = new Problem75();
-            int ans = myProblem.Soln1(120);
-            //int ans = myProblem.Soln1(1500000);
+            //int ans = myProblem.Soln1(50);  // 6
+            //int ans = myProblem.Soln2(50);  // 6
+            //int ans = myProblem.Soln2(120);     // 13
+            int ans = myProblem.Soln2(1500000);
             Console.WriteLine("The answer is {0}.", ans);
         }
 
@@ -32,7 +35,7 @@ namespace Problems70to79
         
         private int FindIntRtTriangles(int L)
         {
-            List<Tuple<int, int, int>> trianges = new List<Tuple<int, int, int>>();
+            List<Triangle> trianges = new List<Triangle>();
             int nFound = 0;
 
             for (int x = 1; x < L; x++)
@@ -40,22 +43,136 @@ namespace Problems70to79
                 {
                     int z = L - x - y;
                     //Console.WriteLine("trying: ({0}, {1}, {2})", x, y, z);
-                    if (x*x + y*y == z*z)
+                    if (Triangle.IsValidTriangle(x, y, z))
                     {
-                        // got it.
-                        trianges.Add(Tuple.Create(x, y, z));
-;                       //Console.WriteLine("{0}cm: ({1}, {2}, {3})", L, x, y, z);
-                        nFound++;
+                        Triangle t = new Triangle(x, y, z);
+                        if (t.IsRightAngle())
+                        {
+                            // got it.
+                            trianges.Add(t);
+                            //Console.WriteLine("{0}cm: ({1}, {2}, {3})", L, x, y, z);
+                            nFound++;
+                        }
                     }
                 }
             if (nFound > 0)
             {
                 Console.Write("{0}cm: ", L);
                 foreach (var t in trianges)
-                    Console.Write("({0}, {1}, {2}) ", t.Item1, t.Item2, t.Item3);
+                    Console.Write(t.ToString());
                 Console.WriteLine();
             }
             return nFound;
+        }
+
+        private int Soln2(int lMax)
+        {
+            List<Triangle>[] trianglesOfL = new List<Triangle>[lMax + 1];
+            int[] nTrianglesOfL = new int[lMax+1];
+
+            for (int k = 1; k < lMax; k++)
+            {
+                Console.WriteLine($"k = {k}...");
+                for (int n = 1; n < lMax; n++)
+                {
+                    Console.WriteLine($"  n = {n}...");
+                    for (int m = n + 1; m < lMax; m++)
+                    {
+                        int a = k * (m * m - n * n);
+                        int b = k * (2 * m * n);
+                        int c = k * (m * m + n * n);
+                        if (a < lMax && b < lMax && c < lMax)
+                        {
+                            long L = a + b + c;
+                            //int L = k * 2 * ((m * m) + (m * n));
+                            if (L <= lMax && Triangle.IsValidTriangle(a, b, c))
+                            {
+                                Triangle t = new Triangle(a, b, c);
+                                if (trianglesOfL[L] is null)
+                                    trianglesOfL[L] = new List<Triangle>();
+                                if (!trianglesOfL[L].Any(t1 => t1.Equals(t)))
+                                {
+                                    //Console.WriteLine($"{L}cm: {t} - k={k}, n={n}, m={m}");
+                                    trianglesOfL[L].Add(t);
+                                    nTrianglesOfL[L]++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            int nTotal = 0;
+            for (int i = 1; i <= lMax; i++)
+                if (nTrianglesOfL[i] >= 1)
+                {
+                    Console.Write("{0}cm: {1} triangles: ", i, nTrianglesOfL[i]);
+                    foreach (Triangle t in trianglesOfL[i])
+                        Console.Write(t.ToString());
+                    Console.WriteLine();
+                    if (nTrianglesOfL[i] == 1)
+                        nTotal++;
+                }
+            return nTotal;
+        }
+
+    }
+
+    internal struct Triangle
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+
+        public Triangle(int x, int y, int z)
+        {
+            if (x <= 0 || y <= 0 || z <= 0)
+                throw new ArgumentException("Sides of a triangle must be positive numbers.");
+            if (x + y <= z || x + z <= y || y + z <= x)
+                throw new ArgumentException("The sum of the lengths of any two sides of a triangle must be greater than the length of the third side.");
+
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static bool IsValidTriangle(int x, int y, int z)
+        {
+            if (x <= 0 || y <= 0 || z <= 0)
+                return false;
+            if (x + y <= z || x + z <= y || y + z <= x)
+                return false;
+            return true;
+        }
+
+        public bool IsRightAngle()
+        {
+            // x² + y² = z²
+            return (X * X + Y * Y == Z * Z);
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            if (obj is Triangle)
+            {
+                Triangle t = (Triangle)obj;
+                int[] thisSides = new int[] { X, Y, Z };
+                int[] otherSides = new int[] { t.X, t.Y, t.Z };
+                Array.Sort(thisSides);
+                Array.Sort(otherSides);
+                return thisSides.SequenceEqual(otherSides);
+                //return (t.X == X && t.Y == Y && t.Z == Z);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (X, Y, Z).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z}) ";
         }
     }
 }
