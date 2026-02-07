daaa5b77bf1ad0611a89e496eac4c8b6dbd7979e
Andrew Huey <andyhuey@users.noreply.github.com>
2026-02-07 15:19:32 -0500

    solved problem 77

--- Euler7/Problems70to79/Problem77.cs
+++ Euler7/Problems70to79/Problem77.cs
@@ -10,12 +10,44 @@ namespace Problems70to79
 {
     internal class Problem77
     {
+        private bool[] primes;
+        private int[] ways;
+
+        public Problem77(int maxN)
+        {
+            primes = Utils.GetPrimes(maxN + 1);
+            ways = GetWays(maxN);
+        }
+
+        private int[] GetWays(int maxN)
+        {
+            int[] ways = new int[maxN + 1];
+            ways[0] = 1;
+            for (int i = 1; i <= maxN; i++)
+                if (primes[i])
+                    for (int j = i; j <= maxN; j++)
+                        ways[j] += ways[j - i];
+            return ways;
+        }
+
         public static void Run()
         {
             Console.WriteLine("Started at {0}", DateTime.Now);
-            var myProblem = new Problem77();
-            //myProblem.HowManyWays(10);
-            myProblem.RunTests();
+            int maxN = 100; // just a guess.
+            var myProblem = new Problem77(maxN);
+            int targetValue = 5000; // "more than 5000 ways."
+            for (int n = 1; n <= maxN; n++)
+            {
+                int numWays = myProblem.HowManyWays(n);
+                if (numWays > targetValue)
+                {
+                    Console.WriteLine("The answer is {0}, which can be written in {1} ways.", 
+                        n, numWays);
+                    break;
+                }
+            }
+            //Console.WriteLine("PS(99) = {0}", myProblem.HowManyWays(99));
+            //myProblem.RunTests();
         }
 
         private void RunTests()
@@ -38,17 +70,6 @@ namespace Problems70to79
 
         private int HowManyWays(int N)
         {
-            // given N, how many ways can it be written as a sum of primes?
-            // if (N < 2) return 0;
-            bool[] primes = Utils.GetPrimes(N + 1);
-            // PrintPrimes(N, primes);
-
-            int[] ways = new int[N + 1];
-            ways[0] = 1;
-            for (int i = 1; i <= N; i++)
-                if (primes[i])
-                    for (int j = i; j <= N; j++)
-                        ways[j] += ways[j - i];
             return ways[N];
         }
 
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # euler
 My Project Euler work.
 
+https://projecteuler.net/
+
 In several Visual Studio projects, about 10 problems per project.
 
-https://projecteuler.net/
+I started working on Project Euler in 2013, with the goal of learning some new stuff and having a little fun.
+
+In some cases, I've copied other people's solutions into my projects, for comparison and experimentation. When I've done that, I've added a comment noting the source.
+
+Starting with problem 77, in 2026, I've started experimenting with both GitHub Copilot and Claude Code to help me figure out the solutions. I'm trying to use AI as a coach here, not to just solve the problem for me.
+The goal is to experiment with coding assistants and learn how to use them effectively.
